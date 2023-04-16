f361f832193360cb0a80427bd1b0ba899410688c
tamoraji <askarzadeh.mojtaba@gmail.com>
2023-04-16 19:32:04 -0400

    Changes to be committed: 	modified:   .DS_Store 	deleted:    CIF.py 	deleted:    HC2.py 	modified:   LR.ipynb 	renamed:    BOSS.py -> LR.py 	deleted:    MRSQM.py 	deleted:    PF_multiprocess.py 	deleted:    RISE.py 	modified:   Results/.DS_Store 	deleted:    RotationForest.py 	deleted:    STC.py 	deleted:    SVM_multiprocess.py 	deleted:    TDE.py 	deleted:    XGBOOST.py

--- LR.py
+++ LR.py
@@ -1,14 +1,38 @@
-from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
 from sklearn.model_selection import cross_val_predict, KFold
 import sklearn
 import numpy as np
 import os
 import sys
 
-
-
 # define a list of datasets
-datasets = ["Control_charts", "PHM2022_Multivar", "PHM2022_Univar_PDIN"]
+datasets = [
+"BEARING_Univar",
+"PHM2022_Multivar",
+"PHM2022_Univar_PIN",
+"PHM2022_Univar_PO",
+"PHM2022_Univar_PDIN",
+"ETCHING_Multivar",
+"MFPT_48KHZ_Univar",
+"MFPT_96KHZ_Univar",
+"PADERBORN_64KHZ_Univar",
+"PADERBORN_4KHZ_Univar",
+"PADERBORN_64KHZ_Multivar",
+"PADERBORN_4KHZ_Multivar",
+"Hydraulic_systems_10HZ_Multivar",
+"Hydraulic_systems_100HZ_Multivar",
+"Gas_sensors_home_activity",
+"Control_charts",
+"CWRU_12k_DE_univar",
+"CWRU_12k_DE_multivar",
+"CWRU_12k_FE_univar",
+"CWRU_12k_FE_multivar",
+"CWRU_48k_DE_univar",
+"CWRU_48k_DE_multivar"
+]
+print("We have {} Datasets for evaluation".format(len(datasets)))
+
+#datasets = ["PHM2022_Multivar", "PHM2022_Univar_PDIN"]
+
 datasets_path = "../datasets"
 
 for dataset in datasets:
@@ -20,16 +44,12 @@ for dataset in datasets:
     Labels_name = dataset + "_Labels"
     Labels = np.load(datasets_path + "/"  + Labels_name + ".npy")
 
-
 # change this directory for your machine
 root_dir = './'
 
-
-# define a list of algorithms
 algorirhms_path = "./classifiers"
 
-from classifiers import HC2_module
-
+from classifiers import LR_module
 
 # define the number of folds
 n_folds = 5
@@ -56,10 +76,9 @@ for dataset in datasets:
 
 
 
-    #Run The HC2 Module
-    HC2_module.HC2(results_path, Dataset_name, Dataset, Labels, nb_folds=n_folds,
-                n_jobs=10)
+    #Run The Logistic Regression (LR) module
+    LR_module.LR(results_path, Dataset_name, Dataset, Labels, nb_folds= n_folds, C=10)
 
 
-    
+
 
